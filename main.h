da9841c760c52e01fcd28e86f27e426cf638986c
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-04-22 17:15:25 -0700

    Handle the following conversion specifiers:

--- main.h
+++ main.h
@@ -5,21 +5,37 @@
 #include <unistd.h>
 #include <stdarg.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
 
 /**
- * struct format - structure of a sign and its function
- * @sign: format specifiers
- * @fun: function pointer.
- *
+ * struct print - print
+ * @parameter: The operator
+ * @f: The function associated
  */
-typedef struct format
+typdef struct printType
 {
-	char *sign;
-	int (*fun)(va_list);
-} format_t;
+	char *parameter;
+	int (*f)(va_list ap);
+} print_type;
 
 int my_printf(const char *format, ...);
 int _putstring(char m);
+int _string(va_list ap);
+int _char(va_list ap);
+int _percent(va_list ap _attribute_((unused)));
+char *convert(unsigned int num, int base);
+int _rot13(va_list ap);
+int _reverse(va_list ap);
+int _int(va_list ap);
+int _unsigned(va_list ap);
+int _octal(va_list);
+int _hex_u(va_list ap);
+int _binary(va_list ap);
+int _hex_1(va_list ap);
+int get_flags(const char *format, print_type argument[], va_list ap);
+int _address(va_list ap);
+
 
 
 #endif
