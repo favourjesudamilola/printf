86f6b7dd984a9b24c7212eefabd047f88940631b
Jesudamilola <jesudamilola2000@gmail.com>
2023-04-24 06:20:28 -0700

    changes

--- printf.c
+++ printf.c
@@ -1,29 +1,45 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _printf - function that prints characters
  * @format - character format
  * Return - number of characters
  */
-int printf(const char *format, ...)
+int my_printf(const char *format, ...)
 {
        	count = 0;
-	va_list args;
-	va_start(args, format);
-
-	while (*format != '\0') {
-		if (*format == '%') {
-			format++;
-
-	count = 0;
 	char c = 'A';
 	int num = 123;
-	char str[] = "Hello, world!";
-	count += printf("Character: %c\n", c);
-	count += printf("Number: %d\n", num);
-	count += printf("String: %s\n", str);
-	
-	printf("Total characters printed: %d\n", count);
+	va_list args;
+
+	va_start(args, format);
+	while (*format != '\0') {
+		if (*format == '%')
+		format++;
+		switch (*format) {
+			case 'c':
+				putchar(va_arg(args, int));
+                                count++;
+                                break;
+                        case 's':
+                                count += puts(va_arg(args, char *));
+                                break;
+                        case 'd':
+                                count += printf("%d", va_arg(args, int));
+                                break;
+                        default:
+                                putchar('%');
+                                putchar(*format);
+                                count += 2;
+                                break;
+               }
+	} else {
+		putchar(*format);
+		count++;
 	}
+
+	printf("Total characters printed: %d\n", count);
+	va_end(args);
 	return count;
 }
--- putstring.c
+++ putstring.c
@@ -9,5 +9,5 @@
 
 int _putstring(char c)
 {
-	return (write(1, &c, 1));
+        return (write(1, &c, 1));
 }
