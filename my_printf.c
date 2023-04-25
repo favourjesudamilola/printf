f792b49d362a7c5619b12b6a9f39a36fc6747fc7
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-04-24 19:28:29 -0700

    hot fixed

--- my_printf.c
+++ my_printf.c
@@ -2,23 +2,26 @@
 
 /**
  * _printf - Function that produces output according to format
- * @format:
+ * @format: a character string
  *
- * Return:
+ * Return: the number of characters printed
  */
 
 int _printf(const char *format, ...)
 {
-	unsigned h = 0, r_value = 0;
+	unsigned h = 0, len2 = 0;
+
 	va_list args;
 	va_start(args, format);
-
 	for (; format[h] != '\0'; h++)
 	{
 		if (format[h] != '%')
 		{
 			_putstring(format[h]);
-			r_value += 1;
+			len2 += 1;
 		}
+		else if (format[h + 1] == 'c')
+			_putstring(va_arg(args, int));
 	}
+	return (len2);
 }
